6296580564db5785eff2555bd847a09037176076
Evstratenko98 <evstratenko.sv98@gmail.com>
2021-12-23 10:54:00 +0300

    delete build

--- build/vendors/js/partner.js
+++
@@ -1,8 +0,0 @@
-ymaps.ready(() => {
-    const Map = new ymaps.Map("yandexMap", {
-        center: [55.76, 37.64],
-        zoom: 10,
-        controls: ["zoomControl"],
-    });
-    Map.behaviors.disable("scrollZoom");
-});
