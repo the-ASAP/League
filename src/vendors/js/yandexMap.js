27166f4209e80d990496f573e7a2dde52ff78bab
Evstratenko98 <evstratenko.sv98@gmail.com>
2021-12-23 10:57:10 +0300

    new yandexMap

--- src/vendors/js/yandexMap.js
+++ src/vendors/js/yandexMap.js
@@ -1,37 +1,40 @@
-export function createHint(maps, address, object, coorArr, link) {
+function createHint(maps, address, object, coorArr, link) {
     let myPlacemark = new maps.Placemark(
-        [coorArr[0], coorArr[1]],
-        {
-            address,
-            object,
-            link,
-            balloonContentHeader: object,
-            balloonContentBody: address,
-            balloonContentFooter: link
-                ? `<a href=${link} class="yandexMap__hint">` + "Перейти" + "</a>"
-                : "",
-        },
-        {
-            // hintLayout: HintLayout,
-            iconColor: "#39ad46",
-        }
+      [coorArr[0], coorArr[1]],
+      {
+        address,
+        object,
+        link,
+        balloonContentHeader: object,
+        balloonContentBody: address,
+        balloonContentFooter: link
+          ? `<a href=${link} class="yandexMap__hint">` + 'Перейти' + '</a>'
+          : ''
+      },
+      {
+        // hintLayout: HintLayout,
+        iconColor: '#e23d3d'
+      }
     );
-
+  
     return myPlacemark;
-}
+  }
 
-ymaps.ready(async () => {
+  
+ymaps.ready(() => {
     const Map = new ymaps.Map("yandexMap", {
         center: [55.76, 37.64],
         zoom: 10,
         controls: ["zoomControl"],
     });
     Map.behaviors.disable("scrollZoom");
-    // const data = await fetch('').then((res) => {
-    // ...
-    // В зависимости от результата, мы выводим новые метки на карте
-    // myMap.geoObjects.add(
-    //      createHint(ymaps, address, object, [latitude, longitude], link)
-    //   );
-    // })
+
+    Map.geoObjects.add(
+        createHint(
+            ymaps,
+          'Москва',
+          'Тут будет туса!',
+          [55.76, 37.64]
+        )
+      );
 });
