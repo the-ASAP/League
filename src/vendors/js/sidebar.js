6ce3f5f425ac01d5418e335b2818e80a889ca597
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2022-06-02 18:24:06 +0400

    fix sidebar-menu logic

--- src/vendors/js/sidebar.js
+++ src/vendors/js/sidebar.js
@@ -1,9 +1,15 @@
 // for price-index pages
 
-$(function () {
-  $(".sidebar__nav [href]").each(function () {
-    if (this.href == window.location.href) {
-      $(this).addClass("active");
-    }
-  });
+// $(function () {
+//   $(".sidebar__nav [href]").each(function () {
+//     if (this.href == window.location.href) {
+//       $(this).addClass("active");
+//     }
+//   });
+// });
+
+$(".sidebar__nav [href]").each(function () {
+  if (window.location.pathname.indexOf($(this).attr("href")) > -1) {
+    $(this).addClass("active");
+  }
 });
